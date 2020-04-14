122c77600bd0b31a2efab30de76025b644bb6e3e
1224AmOgH <46253936+1224AmOgH@users.noreply.github.com>
2020-04-14 13:50:53 +0530

    asd

--- activitiesjs.js
+++ activitiesjs.js
@@ -1,14 +1,19 @@
-var myIndex = 0;
-carousel();
+var slideIndex = [1,1];
+var slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
 
-function carousel() {
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+     x[i].style.display = "none";
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  x[myIndex-1].style.display = "block";
-  setTimeout(carousel, 4000); // Change image every 4 seconds
+  x[slideIndex[no]-1].style.display = "block";
 }
