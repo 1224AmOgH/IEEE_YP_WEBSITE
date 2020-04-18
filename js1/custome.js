8a4978cc7018b81ced775e3977f6aa8550d101f6
1224AmOgH <46253936+1224AmOgH@users.noreply.github.com>
2020-04-18 13:11:16 +0530

    act

--- js1/custome.js
+++ js1/custome.js
@@ -7,7 +7,7 @@ $(' #slider .owl-carousel').owlCarousel({
     margin:10,
     nav:true,
     autoplay:true,
-    autoplayTimeout:1000,
+    autoplayTimeout:4000,
     animateIn: 'fadeIn',
     animateOut: 'fadeOut',
     responsive:{
